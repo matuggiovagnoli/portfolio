54e584605d6f8e44fc92b20569ca7dbd3db84117
Matias Gomez <matiasgomez@3ns.es>
2024-07-12 00:51:23 +0200

    adjusting

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         },
         marquee: {
           '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-103%)' },
+          '100%': { transform: 'translateX(-105%)' },
         },
         enter: {
           '0%': { opacity: 0 },
